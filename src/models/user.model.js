a02a9409fa60d23ae9a3496856436cd08725d7fe
Darshan <pateldarshan0011@gmail.com>
2024-04-27 09:08:18 +0530

    add user register controller and bug fixes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -49,10 +49,10 @@ const userSchema = new Schema(
 
     }
 
-    , { timestamps })
+    , { timestamps: true })
 userSchema.pre("save", async function (next) {
     if (!this.isModified("password")) return next();
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 userSchema.methods.isPasswordCorrect = async function (password) {
